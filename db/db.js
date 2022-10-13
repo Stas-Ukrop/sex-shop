27ed1a1e5ec08bdbfcfc4c5d93316ffb7bb1e478
Stas-Ukrop <st.kr342@gmail.com>
2022-10-13 21:58:23 +0200

    add mongoose

--- db/db.js
+++ db/db.js
@@ -1,19 +1,29 @@
-import mongodb from 'mongodb'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv/config'
 
 const URI_DB = process.env.URI_DB
 
-const myDB = mongodb.MongoClient.connect(
+const myDB = mongoose.MongoClient.connect(
     URI_DB,
     {
+        useUnifiedTopology:true,
         maxPoolSize: 5
     }
 )
+mongoose.connection.on('connected', () => {
+    console.log(`Connection open ${URI_DB}`)
+})
+mongoose.connection.on('error', (e) => {
+    console.log(`Error mongoose connection ${e.message}`)
+})
+mongoose.connection.on('disconnected', (e) => {
+    console.log(`Mongoose disconnected`)
+})
 process.on('SIGINT', async () => {
-    const db = await myDB
-    db.close()
-    console.log('время спатоньки')
-    process.exit(1)
+    mongoose.connection.close(() => {
+        console.log('время спатоньки')
+        process.exit(1)        
+    })
 })
 
 export default myDB
